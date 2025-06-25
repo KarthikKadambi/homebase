266ce1d48ccf88c5332d6230adafd5bc09b1655a
Karthik Kadambi <krtkram@gmail.com>
2025-06-24 21:22:40 -0700

    Update config to use HtmlBasePlugin

---
+++ README.md
@@ -0,0 +1,6 @@
+# Eleventy
+
+## Folder structure
+_data - global data for 11ty to use on templates.
+_includes - reusable partials and layouts for eleventy to use on templates.
+images - images that are used on the site.
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,14 @@
+import { HtmlBasePlugin } from '@11ty/eleventy';
+
 /**
  * @param {import('@11ty/eleventy/UserConfig').default} eleventyConfig
  */
 export default async function(eleventyConfig) {
     eleventyConfig.setInputDirectory('src');
     eleventyConfig.setOutputDirectory('dist');
+    eleventyConfig.addPlugin(HtmlBasePlugin, {
+        baseHref: 'https://karthikkadambi.com/',
+    });
     return {
         markdownTemplateEngine: 'njk',
         htmlTemplateEngine: 'njk',
