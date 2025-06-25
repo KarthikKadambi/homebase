7a5c33723a674d25326a01088cba3b3f3e73d5c3
Karthik Kadambi <krtkram@gmail.com>
2025-06-24 22:11:07 -0700

    Add Atkinson hypereligible fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,7 @@ export default async function(eleventyConfig) {
     eleventyConfig.setInputDirectory('src');
     eleventyConfig.setOutputDirectory('dist');
     eleventyConfig.addPassthroughCopy('src/images');
+    eleventyConfig.addPassthroughCopy('src/fonts');
     eleventyConfig.addPlugin(HtmlBasePlugin, {
         baseHref: 'https://karthikkadambi.com/',
     });
