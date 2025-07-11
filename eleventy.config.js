96ec4d16cb8d86b59b3cf4de35e5e14f12e67b51
Karthik Kadambi <krtkram@gmail.com>
2025-07-10 21:59:13 -0700

    Restructure images and webmanifest

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,6 +9,7 @@ export default async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/fonts');
     eleventyConfig.addPassthroughCopy('src/images');
     eleventyConfig.addPassthroughCopy('src/robots.txt');
+    eleventyConfig.addPassthroughCopy('src/site.webmanifest');
     eleventyConfig.addWatchTarget("src/css");
     eleventyConfig.addPlugin(HtmlBasePlugin);
     return {
