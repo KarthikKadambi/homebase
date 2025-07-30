0254ad1f03a06f2ccd7bdfc4b35c110630530451
Karthik Kadambi <krtkram@gmail.com>
2025-07-29 21:34:34 -0700

    Cleanup

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,12 +6,16 @@ import { HtmlBasePlugin } from '@11ty/eleventy';
 export default async function(eleventyConfig) {
     eleventyConfig.setInputDirectory('src');
     eleventyConfig.setOutputDirectory('dist');
+    
     eleventyConfig.addPassthroughCopy('src/fonts');
     eleventyConfig.addPassthroughCopy('src/images');
     eleventyConfig.addPassthroughCopy('src/robots.txt');
     eleventyConfig.addPassthroughCopy('src/site.webmanifest');
+    
     eleventyConfig.addWatchTarget("src/css");
+    
     eleventyConfig.addPlugin(HtmlBasePlugin);
+    
     return {
         markdownTemplateEngine: 'njk',
         htmlTemplateEngine: 'njk',
