0441525299adc377c5d0a2ce94cd2247514a4710
Karthik Kadambi <krtkram@gmail.com>
2025-08-11 20:46:15 -0700

    Add postcss import ext glob

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default {
   plugins: {
+    'postcss-import-ext-glob': {},
     'postcss-import': {},
     'tailwindcss': {},
     ...(isProd && { 'cssnano': {} })
