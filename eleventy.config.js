ff7fe98dd02019817c1dae73913dd58df866f328
Karthik Kadambi <krtkram@gmail.com>
2025-08-13 20:28:26 -0700

    Add atom feed support

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 import { HtmlBasePlugin } from '@11ty/eleventy';
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 /**
  * @param {import('@11ty/eleventy/UserConfig').default} eleventyConfig
@@ -15,6 +16,25 @@ export default async function(eleventyConfig) {
     eleventyConfig.addPlugin(HtmlBasePlugin);
 
     eleventyConfig.addWatchTarget("src/css");
+
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed/feed.xml",
+		collection: {
+			name: "posts", // iterate over `collections.posts`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Karthik Kadambi's Blog",
+			subtitle: "Learn about web development, and more by Karthik Kadambi, full stack web developer",
+			base: "https://karthikkadambi.com/",
+			author: {
+				name: "Karthik Kadambi",
+				email: "krtkram@gmail.com", // Optional
+			},
+		}
+	});
     
     return {
         markdownTemplateEngine: 'njk',
